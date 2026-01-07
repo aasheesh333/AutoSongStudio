070bb19a526e28d0ae6e1bbef3eb6979f94c8ad3
Aasheesh Singh <aasheesh333@gmail.com>
2026-01-07 05:36:41 +0000

    fix: correct module imports in videoGenerationWorker

--- backend/workers/videoGenerationWorker.js
+++ backend/workers/videoGenerationWorker.js
@@ -1,8 +1,6 @@
 const config = require('../config');
-const VideoModel = require('../models/Video');
-const SchedulerModel = require('../models/Scheduler');
-const UserModel = require('../models/User');
-const SunoKeyUsageModel = require('../models/SunoKeyUsage');
+const config = require('../config');
+const { VideoModel, SchedulerModel, UserModel, SunoKeyUsageModel } = require('../models');
 const groqService = require('../services/groqService');
 const sunoService = require('../services/sunoService');
 const imageService = require('../services/imageGenerationService');
